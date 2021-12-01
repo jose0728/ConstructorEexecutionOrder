65bcd3a4a5dd77ddbca9b5aa845b7eea2255b463
fanyu <fanyu@zhijiaxing.net>
2021-12-01 15:07:36 +0800

    构造函数执行顺序demo

---
+++ 构造函数执行顺序/MyBaseClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 构造函数执行顺序
+{
+    public class MyBaseClass
+    {
+        public MyBaseClass()
+        {
+            Console.WriteLine("Base 0");
+        }
+
+        public MyBaseClass(int i)
+            : this()
+        {
+            Console.WriteLine("Base 1");
+        }
+
+        public MyBaseClass(int i, int j)
+        {
+            Console.WriteLine("Base 2");
+        }
+    }
+
+
+    public class MyDrivedClass : MyBaseClass
+    {
+        public MyDrivedClass()
+            : base(0)
+        {
+            Console.WriteLine("Drived 0");
+        }
+
+        public MyDrivedClass(int i)
+        {
+            Console.WriteLine("Drived 1");
+        }
+
+        public MyDrivedClass(int i, int j)
+            : base(i, j)
+        {
+            Console.WriteLine("Drived 2");
+        }
+    }
+}
---
+++ 构造函数执行顺序/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 构造函数执行顺序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //执行顺序System.Object()->MyBaseClass()->MyBaseClass(int i)->MyDrivedClass()
+            MyDrivedClass md = new MyDrivedClass();
+
+            //执行顺序System.Object()->MyBaseClass()->MyDrivedClass(int i)
+            int i = 0;
+            MyDrivedClass md1 = new MyDrivedClass(i);
+
+            //若要不执行默认构造函数，需要添加继承base(canshu)或this(canshu)
+            //执行顺序System.Object()->MyBaseClass()->MyBaseClass(int i)->MyDrivedClass(int i, int j)
+            MyDrivedClass mdc = new MyDrivedClass(7, 8);
+        }
+    }
+}
